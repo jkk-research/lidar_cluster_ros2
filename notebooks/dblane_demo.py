abadf214ca345602a3e5e023bc3f14183462dc7b
umiklos <ungermiklos1992@gmail.com>
2024-01-05 13:11:08 +0100

    add rectengular search for calc_unassigned

--- notebooks/dblane_demo.py
+++ notebooks/dblane_demo.py
@@ -15,13 +15,13 @@ class Point:
         cluster_id = 1: assigned to the first cluster
         cluster_id = 2: assigned to the second cluster and so on
         """
-        self.cluster_id = cluster_id # cluster ID  
+        self.cluster_id = cluster_id # cluster ID
         self.neighbor_pts = neighbor_pts # neighbor points
 
 # define a class of clusters
 class Cluster:
     """A class to represent a cluster.
-    
+
     Attributes
     ----------
     candidate_points : list
@@ -71,7 +71,7 @@ class Cluster:
                 last2 = self.cluster_points[i][-1]
                 # calculate the angle of the first and last points
                 self.head_angle[i] = self.calculate_angle(first1, first2)
-                self.tail_angle[i] = self.calculate_angle(last1, last2) 
+                self.tail_angle[i] = self.calculate_angle(last1, last2)
             # if zero or one point in the cluster, set the slope to 0
             else:
                 self.head_angle[i] = 0.0
@@ -89,6 +89,49 @@ class Cluster:
                 else:
                     p.core = False
 
+    
+    def ray_tracing(self,x,y,poly_x,poly_y):
+        n = len(poly_x)
+        inside = False
+        p2x = 0.0
+        p2y = 0.0
+        xints = 0.0
+        p1x = poly_x[0]
+        p1y = poly_y[0]
+        for i in range(n+1):
+            p2x = poly_x[i % n]
+            p2y = poly_y[i % n]
+            if y > min(p1y,p2y):
+                if y <= max(p1y,p2y):
+                    if x <= max(p1x,p2x):
+                        if p1y != p2y:
+                            xints = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                        if p1x == p2x or x <= xints:
+                            inside = not inside
+            p1x,p1y = p2x,p2y
+
+        return inside
+    
+    def extract_poligon(self,points,rectangle_width):
+        xleft=[]
+        xright=[]
+
+        x = [p.x for p in points]
+        y = [p.y for p in points]
+
+        for i in x:
+            x1=i-rectangle_width
+            x2=i+rectangle_width
+            xleft.append(x1)
+            xright.append(x2)
+
+        x_all=xleft+xright[::-1]
+        y_all=y+y[::-1]   
+
+        plt.plot(x_all,y_all)
+
+        return x_all,y_all
+
     def set_cluster_points(self, points, cluster_id):
         self.cluster_points[cluster_id] = points
         self.recalculate_head_tail_angles()
@@ -96,28 +139,28 @@ class Cluster:
         self.recalculate_head_tail_angles()
         if len(self.cluster_points[id]) < 2:
             return 0.0, 0.0, 0.0, 0.0, 0.0
-        x_end_t = 1.0 * np.cos(self.tail_angle[id]) + self.cluster_points[id][-1].x 
-        y_end_t = 1.0 * np.sin(self.tail_angle[id]) + self.cluster_points[id][-1].y     
-        print("tail: [%.1f, %.1f] [%.1f, %1.f]" % (self.cluster_points[id][-1].x, self.cluster_points[id][-1].y, x_end_t, y_end_t))   
+        x_end_t = 1.0 * np.cos(self.tail_angle[id]) + self.cluster_points[id][-1].x
+        y_end_t = 1.0 * np.sin(self.tail_angle[id]) + self.cluster_points[id][-1].y
+        print("tail: [%.1f, %.1f] [%.1f, %1.f]" % (self.cluster_points[id][-1].x, self.cluster_points[id][-1].y, x_end_t, y_end_t))
         return self.cluster_points[id][-1].x, self.cluster_points[id][-1].y, x_end_t, y_end_t, self.tail_angle[id]
-    def get_head(self, id): 
+    def get_head(self, id):
         self.recalculate_head_tail_angles()
         if len(self.cluster_points[id]) < 2:
             return 0.0, 0.0, 0.0, 0.0, 0.0
         x_end_h = 1.0 * np.cos(self.head_angle[id]) + self.cluster_points[id][0].x
         y_end_h = 1.0 * np.sin(self.head_angle[id]) + self.cluster_points[id][0].y
-        print("head: [%.1f, %.1f] [%.1f, %1.f]" % (self.cluster_points[id][0].x, self.cluster_points[id][0].y, x_end_h, y_end_h))   
+        print("head: [%.1f, %.1f] [%.1f, %1.f]" % (self.cluster_points[id][0].x, self.cluster_points[id][0].y, x_end_h, y_end_h))
         return self.cluster_points[id][0].x, self.cluster_points[id][0].y, x_end_h, y_end_h, self.head_angle[id]
     # calculate the difference between two angles
     def angle_diff(self, a, b):
         while a < 0.0:
             a += np.pi * 2
         while a > np.pi * 2:
-            a -= np.pi * 2        
+            a -= np.pi * 2
         while b < 0.0:
             b += np.pi * 2
         while b > np.pi * 2:
-            b -= np.pi * 2       
+            b -= np.pi * 2
         if a > b:
             greater = a
             smaller = b
@@ -148,7 +191,7 @@ class Cluster:
         tail_x = self.cluster_points[id][-1].x
         tail_y = self.cluster_points[id][-1].y
         p = Point(tail_x, tail_y)
-        for q in self.candidate_points: 
+        for q in self.candidate_points:
             if q.core == True and q.cluster_id == -1:
                 if self.eps_min <= self.distance(p, q) <= self.eps_max:
                     candidate_angle = self.calculate_angle(p, q)
@@ -168,11 +211,14 @@ class Cluster:
         return np.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2)
     # extend the unassigned values (cluster_id=0) which is already visited, but nut assigned to any cluster
     def calc_unassigned(self, id):
-        for p in self.cluster_points[id]:
-            for q in self.candidate_points:
-                # TODO: self.eps_max * 0.5 is a magic number, should be used as a parameter or calculated from the data
-                if self.distance(p, q) <= self.eps_max * 0.5 and q.cluster_id == -1:
-                    q.cluster_id = 0 # unassigned
+
+        x_all,y_all=self.extract_poligon(self.cluster_points[id],0.5)
+               
+        for q in self.candidate_points:     
+            if self.ray_tracing(q.x,q.y,x_all,y_all) and q.cluster_id == -1:
+                q.cluster_id = 0
+        
+
     def init_new_cluster(self, id):
         # self.cluster_points[id] = []
         for p in self.candidate_points:
@@ -218,15 +264,15 @@ def plot_data(points, plot_neighbor_and_core=False, fig = None, ax = None):
     cluster_id = [p.cluster_id for p in points]
     neighbor_pts = [p.neighbor_pts ** 2 * 20 for p in points]
     cmap1 = plt.get_cmap('summer', np.max(core) + 1)
-    cmap2 = plt.get_cmap('turbo', lut=(np.max(cluster_id) + 2)) # 'CMRmap_r' 'viridis'
+    #cmap2 = plt.get_cmap('turbo', lut=(np.max(cluster_id) + 2)) # 'CMRmap_r' 'viridis'
     if plot_neighbor_and_core == True:
-        plt.scatter(x, y, s=neighbor_pts, c=core, alpha=0.5, cmap=cmap1) ## plot the neighbor points and core points
-        plt.colorbar(shrink=0.4)
-    plt.scatter(x, y, s=50, c=cluster_id, alpha=0.8, cmap=cmap2) ## plot the clusters  ## , vmin=-1, vmax=(np.max(cluster_id)+1)
-    plt.colorbar(cax=None, ax=None, shrink=0.4)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.title('DBlane Demo')
+        plt.scatter(x, y, s=neighbor_pts, c=core, alpha=0.5) #cmap=cmap1) ## plot the neighbor points and core points
+        #plt.colorbar(shrink=0.4)
+    plt.scatter(x, y, s=50, c=cluster_id, alpha=0.8) #cmap=cmap2) ## plot the clusters  ## , vmin=-1, vmax=(np.max(cluster_id)+1)
+    #plt.colorbar(cax=None, ax=None, shrink=0.4)
+    plt.xlabel('x',fontsize=20)
+    plt.ylabel('y',fontsize=20)
+    plt.title('DBlane Demo',fontsize=20)
     plt.axis('equal')
     plt.grid()
 
@@ -246,8 +292,8 @@ def plot_cluster():
     global my_cluster, cluster_plot, tail_plot, head_plot
     # list comprehension
     x = [p.x for p in my_cluster.cluster_points[0]]
-    y = [p.y for p in my_cluster.cluster_points[0]]   
-    cluster_plot.set_xdata(x) 
+    y = [p.y for p in my_cluster.cluster_points[0]]
+    cluster_plot.set_xdata(x)
     cluster_plot.set_ydata(y)
 
     head_start_x, head_start_y, head_end_x, head_end_y, head_angle = my_cluster.get_head(id=1)
@@ -268,7 +314,7 @@ def plt_cluster(cluster, fig = None, ax = None, c_id=1):
     plt.plot([head_start_x, head_end_x], [head_start_y, head_end_y], 'b*-', label='head_plot', alpha=0.4, linewidth=4.2)
     plt.plot([tail_start_x, tail_end_x], [tail_start_y, tail_end_y], 'y*-', label='tail_plot', alpha=0.4, linewidth=4.2)
     x = [p.x for p in cluster.cluster_points[c_id]]
-    y = [p.y for p in cluster.cluster_points[c_id]]   
+    y = [p.y for p in cluster.cluster_points[c_id]]
     plt.plot(x,y, 'r.-', label='cluster_plot', alpha=0.4, linewidth=4.2)
     if fig == None:
         plt.legend()
