4e654594eac624fa7663bc0b41d931f597bb12ac
horverno <horverno@gmail.com>
2024-01-09 14:06:18 +0100

    First attempt to implement DBSCAN python code as c++ node

--- launch/dbscan_spatial.launch.py
+++ launch/dbscan_spatial.launch.py
@@ -1,2 +1,24 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+
+
+
+    return LaunchDescription([
+        Node(
+            package='lidar_cluster',
+            executable='dbscan_spatial',
+            output='screen',
+            parameters=[
+                {'points_in_topic': 'nonground'},
+                {'points_out_topic': 'clustered_points'},
+                {'marker_out_topic': 'clustered_marker'},
+                {'verbose1': True},
+            ]
+        )
+    ])
--- src/dbscan_spatial.cpp
+++ src/dbscan_spatial.cpp
@@ -5,6 +5,8 @@
 #include <functional>
 #include <memory>
 #include <string>
+#include <cmath>
+#include <vector>
 // ROS
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
@@ -19,6 +21,77 @@
 using namespace std::chrono_literals;
 using std::placeholders::_1;
 
+class Point
+{
+public:
+  double x, y;
+  int neighbor_pts = 0;
+  bool core = false;
+  int cluster_id = -1;
+};
+
+double distance(const Point &p1, const Point &p2)
+{
+  return std::sqrt(std::pow(p1.x - p2.x, 2) + std::pow(p1.y - p2.y, 2));
+}
+
+void find_neighbors(std::vector<Point> &points, double eps)
+{
+  for (Point &p : points)
+  {
+    for (const Point &q : points)
+    {
+      if (distance(p, q) <= eps)
+      {
+        p.neighbor_pts++;
+      }
+    }
+    p.core = p.neighbor_pts >= 3;
+  }
+}
+
+void find_clusters(std::vector<Point> &points, double eps)
+{
+  int actual_cluster_id = 0;
+  for (int i = 0; i < 10; i++)
+  {
+    for (Point &p : points)
+    {
+      if (p.core && p.cluster_id == -1)
+      {
+        p.cluster_id = actual_cluster_id;
+        break;
+      }
+    }
+    for (Point &p : points)
+    {
+      if (p.cluster_id == actual_cluster_id)
+      {
+        for (Point &q : points)
+        {
+          if (q.core && q.cluster_id == -1 && distance(p, q) <= eps)
+          {
+            q.cluster_id = actual_cluster_id;
+          }
+        }
+      }
+    }
+    actual_cluster_id++;
+  }
+  for (Point &p : points)
+  {
+    if (!p.core)
+    {
+      for (const Point &q : points)
+      {
+        if (q.core && q.cluster_id != -1 && distance(p, q) <= eps)
+        {
+          p.cluster_id = q.cluster_id;
+        }
+      }
+    }
+  }
+}
 class DbscanSpatial : public rclcpp::Node
 {
   rcl_interfaces::msg::SetParametersResult parametersCallback(const std::vector<rclcpp::Parameter> &parameters)
@@ -36,7 +109,6 @@ class DbscanSpatial : public rclcpp::Node
       if (param.get_name() == "minY")
       {
         minY = param.as_double();
-
       }
       if (param.get_name() == "minZ")
       {
@@ -76,7 +148,7 @@ class DbscanSpatial : public rclcpp::Node
 public:
   DbscanSpatial() : Node("dbscan_spatial"), count_(0)
   {
-    this->declare_parameter<float>("minX", minX); 
+    this->declare_parameter<float>("minX", minX);
     this->declare_parameter<float>("minY", minY);
     this->declare_parameter<float>("minZ", minZ);
     this->declare_parameter<float>("maxX", maxX);
@@ -85,6 +157,8 @@ public:
     this->declare_parameter<std::string>("points_in_topic", "/lexus3/os_center/points");
     this->declare_parameter<std::string>("points_out_topic", "cluster_points");
     this->declare_parameter<std::string>("marker_out_topic", "cluster_marker");
+    this->declare_parameter<bool>("verbose1", verbose1);
+    this->declare_parameter<bool>("verbose2", verbose2);
     this->get_parameter("minX", minX);
     this->get_parameter("minY", minY);
     this->get_parameter("minZ", minZ);
@@ -94,13 +168,15 @@ public:
     this->get_parameter("points_in_topic", points_in_topic);
     this->get_parameter("points_out_topic", points_out_topic);
     this->get_parameter("marker_out_topic", marker_out_topic);
-
+    this->get_parameter("verbose1", verbose1);
+    this->get_parameter("verbose2", verbose2);
 
     pub_lidar_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(points_out_topic, 10);
     pub_marker_ = this->create_publisher<visualization_msgs::msg::MarkerArray>(marker_out_topic, 10);
-    sub_lidar_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(points_in_topic, rclcpp::SensorDataQoS().keep_last(1), std::bind(&DbscanSpatial::lidar_callback, this, std::placeholders::_1));
+    // TODO: QoS // rclcpp::SensorDataQoS().keep_last(1)
+    sub_lidar_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(points_in_topic, 10, std::bind(&DbscanSpatial::lidar_callback, this, std::placeholders::_1));
     callback_handle_ = this->add_on_set_parameters_callback(std::bind(&DbscanSpatial::parametersCallback, this, std::placeholders::_1));
-    
+
     RCLCPP_INFO(this->get_logger(), "DbscanSpatial node has been started.");
     RCLCPP_INFO(this->get_logger(), "Subscribing to: '%s'", points_in_topic.c_str());
     RCLCPP_INFO(this->get_logger(), "Publishing to: '%s' and '%s'", points_out_topic.c_str(), marker_out_topic.c_str());
@@ -109,24 +185,57 @@ public:
 private:
   void lidar_callback(const sensor_msgs::msg::PointCloud2::ConstSharedPtr input_msg)
   {
-    // RCLCPP_INFO(this->get_logger(), "frame_id: '%s'", input_msg->header.frame_id.c_str());
-
-    // Define min and max for X, Y and Z (crop box - rectangular estimation of non-ground points in front of the car)
-
-    // Filter point cloud data
+    // Convert to PCL data type
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
     pcl::fromROSMsg(*input_msg, *cloud);
     pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZI>);
-    pcl::CropBox<pcl::PointXYZI> crop;
-    crop.setInputCloud(cloud);
-    // Filter out points outside of the box
-    crop.setMin(Eigen::Vector4f(minX, minY, minZ, 1.0));
-    crop.setMax(Eigen::Vector4f(maxX, maxY, maxZ, 1.0));
-    crop.filter(*cloud_filtered);
+    
+    // print the length of the point cloud
+    if (verbose1)
+    {
+      RCLCPP_INFO_STREAM(this->get_logger(), "PointCloud in: " << cloud->width * cloud->height << " data points");
+    }
+
+    // DBSCAN
+    // find neighbors in cloud 
+    std::vector<Point> points;
+
+    int i = 0;
+
+    for (const pcl::PointXYZI &p : cloud->points)
+    {
+      // temporary filter only out a small portion of the point cloud, to work in real time // TODO: fix
+      if (i > 60000 and i < 70000)
+      {
+        Point point;
+        point.x = p.x;
+        point.y = p.y;
+        points.push_back(point);
+      }
+      i++;
+    }
+
+
+    find_neighbors(points, 3.5);
+    // find clusters in cloud
+    find_clusters(points, 3.5);
+    
+    // convert to PointXYZI
+    for (const Point &p : points)
+    {
+      pcl::PointXYZI point;
+      point.x = p.x;
+      point.y = p.y;
+      point.z = 0.0;
+      point.intensity = p.cluster_id;
+      cloud_filtered->points.push_back(point);
+    }
+    
+
     // Convert to ROS data type
     sensor_msgs::msg::PointCloud2 output_msg;
     pcl::toROSMsg(*cloud_filtered, output_msg);
-    // Add the same frame_id as th input, it is not included in pcl PointXYZI
+    // Add the same frame_id as the input, it is not included in pcl PointXYZI
     output_msg.header.frame_id = input_msg->header.frame_id;
     // Publish the data as a ROS message
     pub_lidar_->publish(output_msg);
@@ -138,7 +247,8 @@ private:
   OnSetParametersCallbackHandle::SharedPtr callback_handle_;
   float minX = 0.0, minY = -5.0, minZ = -2.0;
   float maxX = 40.0, maxY = +5.0, maxZ = -0.15;
-  std::string points_in_topic, points_out_topic, marker_out_topic;  
+  bool verbose1 = false, verbose2 = false;
+  std::string points_in_topic, points_out_topic, marker_out_topic;
   size_t count_;
 };
 
