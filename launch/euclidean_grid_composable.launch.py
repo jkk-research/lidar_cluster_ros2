919917597c636f48d45f9135e4a95f8309bfb2ff
umiklos <ungermiklos1992@gmail.com>
2024-12-02 15:08:56 +0100

    fix composable node issue

--- launch/euclidean_grid_composable.launch.py
+++ launch/euclidean_grid_composable.launch.py
@@ -1,38 +1,33 @@
-
 from launch import LaunchDescription
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
 
 def generate_launch_description():
+
     container = ComposableNodeContainer(
         name='euclidean_grid_container',
-        namespace='cluster',
+        namespace='/euc',
         package='rclcpp_components',
-        executable='component_container',
+        executable='component_container_mt',
         composable_node_descriptions=[
             ComposableNode(
                 package='lidar_cluster',
-                plugin='cluster::EuclideanGrid',
-                name='euclidean_grid',
-                parameters=[{
-                    'minX': -80.0,
-                    'minY': -25.0,
-                    'minZ': -2.0,
-                    'maxX': 80.0,
-                    'maxY': 25.0,
-                    'maxZ': -0.15,
-                    'points_in_topic': '/lexus3/os_center/points',
-                    'points_out_topic': 'clustered_points',
-                    'marker_out_topic': 'clustered_marker',
-                    'verbose1': False,
-                    'verbose2': False,
-                    'pub_undecided': False,
-                    'voxel_leaf_size': 3.0,
-                    'tolerance': 5.0,
-                    'max_cluster_size': 400,
-                    'min_points_number_per_voxel': 5
-                }]
-            ),
+                plugin='EuclideanGrid',
+                name='euclidean_grid_filter_composable_unique',
+                #namespace='euclidean_grid_filter_ns',
+                extra_arguments=[{'use_intra_process_comms': True}],
+                parameters=[
+                {'points_in_topic': 'lexus3/os_center/points',
+                'points_out_topic': 'clustered_points',
+                'marker_out_topic': 'clustered_marker',
+                'tolerance': 5.0,
+                'max_cluster_size': 4000,
+                'voxel_leaf_size': 3.0,
+                'min_points_number_per_voxel': 5,
+                'verbose1': False,
+                'verbose2': False,}
+            ],
+            )
         ],
         output='screen',
     )
