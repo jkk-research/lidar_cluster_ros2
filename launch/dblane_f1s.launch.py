22f84322eb1d8ec13aa080eb6900cecfadc9d478
umiklos <ungermiklos1992@gmail.com>
2026-01-08 14:54:45 +0100

    Update dependencies and launch defaults

--- launch/dblane_f1s.launch.py
+++ launch/dblane_f1s.launch.py
@@ -19,12 +19,19 @@ def generate_launch_description():
                 {'points_in_topic': LaunchConfiguration("topic")},
                 {'points_out_topic': 'clustered_points'},
                 {'marker_out_topic': 'clustered_marker'},
+                {'minX': -20.0},
+                {'minY': -20.0},
+                {'maxX': 0.0},
+                {'maxY': 20.0},
+                {'maxZ': 0.1},
+                {'minZ': -0.1},
                 {'verbose1': False},
                 {'search_start_width_x': 20.0},
                 {'search_start_width_y': 6.5},
-                {'eps_min': 2.2},
-                {'eps_max': 5.0},
+                {'eps_min': 0.2},
+                {'eps_max': 5.5},
                 {'ang_threshold_deg': 45.0},
+                {'origin_filter_radius': 0.25},
             ]
         )
     ])
